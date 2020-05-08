a46b801ac5f3c14f24b76a09426e442fc1cec9c5
uma Prithi <umaprithi@outlook.com>
2020-05-08 19:06:10 +0200

    Showing dialog box when an item is added

--- Labb1/wwwroot/js/site.js
+++ Labb1/wwwroot/js/site.js
@@ -2,14 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function pageLoad() {
+    alert('You Clicked Button');
+}
+
+function test() {
+    document.getElementById('textfield').innerHTML='text changed'
+}
 
 function DisplayResponseMessage(productName) {
 
     // If productName is null, stop here! Dont show any message
     if (productName == null)
         return;
+   
 
     document.getElementById('item_added_to_cart').innerHTML = productName;
     $('#exampleModalCenter').modal('show');
 }
 
+
+
+function addtocart(productid) {
+      console.log(productId);
+
+    //fetch("https://localhost:44333/ShoppingCart/AddToCart?productid=" + productid)  {
+    //    method: "POST"
+    //};
+   
+        //.then((response) => {
+
+        //    // If response was ok, update cart and display a message with the product name telling it was added to cart.
+        //    if (response.ok) {
+        //        UpdateCartButton(); 
+        //        DisplayResponseMessage(name);
+        //        IncreaseSingleProductInCart(productId);
+        //    } else {
+        //        alert("Skit också, något gick fel. Försök igen eller kontakta vår sketna support!");
+        //    }
+        //});
+
+}
