8fd7a84c7e8cf803fbc195cf79b8927409b17b87
uma Prithi <umaprithi@outlook.com>
2020-05-17 19:46:55 +0200

    Initial setup labb2

---
+++ ProductsService/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsService.Models
+{
+    public class Product
+    {
+        public string productName { get; set; }
+        public Guid id { get; set; }
+        public string description { get; set; }
+        public string color { get; set; }
+        public DateTime publishDate { get; set; }
+        public decimal price { get; set; }
+        public string photo { get; set; }
+    }
+}
---
+++ ProductsService/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsService.Repositories
+{
+    public class ProductRepository
+    {
+        public Product GetById(Guid Id)
+        {
+            return new Product();
+        }
+    }
+}
